0f80fd691cd632f06895f0dd884ed633bb27f0d3
Shajib96 <shajibahamedornob@gmail.com>
2023-09-24 23:29:32 +0600

    solved one

--- problem-1.js
+++ problem-1.js
@@ -1,6 +1,15 @@
 //  check a numer that is integer or not
 function isInteger(number){
-      if(number !=="number"){
-        console.log
+      if(typeof number !=="number"){
+        return'Please input a number'
       }
+      if(number%1==0){
+        return true;
+      }
+      else{
+        return false;
+      }
+      
 }
+var inputNumber= isInteger(6.9)
+console.log(inputNumber)
